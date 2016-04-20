cbaae3ab7f7c2acafea0b881938215392702ac1e
YongJin-Cho <drajin.cho@gmail.com>
2016-04-20 09:56:35 +0900

    adding "Sugar Home" android app(initial version)

---
+++ android/app/src/main/java/com/gaiakeeper/sugarhome/SugarHomeMain.java
@@ -0,0 +1,13 @@
+package com.gaiakeeper.sugarhome;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SugarHomeMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sugar_home_main);
+    }
+}
